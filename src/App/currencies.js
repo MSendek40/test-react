a0d062d4aea64129a3f090cae22793f0d3e7da2b
Marek Sendek <marek.sendek@gmail.com>
2023-01-22 03:32:50 +0100

    changes in structure of document

---
+++ src/App/currencies.js
@@ -0,0 +1,19 @@
+const currencies = [
+    {
+        short: "USD",
+        name: "Dolar amerykański",
+        rate: 4.5235,
+    },
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.6904,
+    },
+    {
+        short: "GBP",
+        name: "Funt brytyjski",
+        rate: 5.4279,
+    },
+];
+
+export default currencies
